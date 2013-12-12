25a304d091e55e2940388fdc71ffb25de249a67b
Jeremie Kaszub <jeremie.kaszub@gmail.com>
2013-12-12 19:10:40 +0100

    Fin des fonctionnalites liees aux utilisateurs

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
 	factory :user do
-		name		"Jeremie Kaszub"
-		email		"jkaszub@example.com"
+		sequence(:name)  { |n| "Person #{n}" }
+		sequence(:email) { |n| "person_#{n}@example.com" }
 		password	"ginger"
 		password_confirmation "ginger"
+
+		factory :admin do
+			admin true
+		end
 	end
 end
